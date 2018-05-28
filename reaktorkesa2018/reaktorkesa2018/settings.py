adeabb423325002c7c2bce149ba61de66c577dbd
Janne Hyttinen <hyttijan@cs.helsinki.fi>
2018-05-28 19:34:56 +0300

    removed allowed hosts

--- reaktorkesa2018/reaktorkesa2018/settings.py
+++ reaktorkesa2018/reaktorkesa2018/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','192.168.1.2']
+ALLOWED_HOSTS = []
 
 
 # Application definition
